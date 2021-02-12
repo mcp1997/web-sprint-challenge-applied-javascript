d5d1922fe09741d6b8e5f33946a8faa621f7625a
Matt Phillips <mattcphillips17@gmail.com>
2021-02-12 14:57:18 -0700

    finished sprint challenge

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,25 @@
 const Header = (title, date, temp) => {
+  
+  const main = document.createElement('div');
+  const mainDate = document.createElement('span');
+  const mainTitle = document.createElement('h1');
+  const mainTemp = document.createElement('span');
+
+  main.classList.add('header');
+  mainDate.classList.add('date');
+  mainTemp.classList.add('temp');
+
+  main.appendChild(mainDate);
+  main.appendChild(mainTitle);
+  main.appendChild(mainTemp);
+
+  mainDate.textContent = date;
+  mainTitle.textContent = title;
+  mainTemp.textContent = temp;
+
+  return main;
+
+
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -13,7 +34,14 @@ const Header = (title, date, temp) => {
   //
 }
 
+
 const headerAppender = (selector) => {
+  
+  const newHeader = Header("Apparently, dogs can now code (and write articles about it)", "Feb 12th, 2021", "63°");
+  const parent = document.querySelector(selector);
+  parent.appendChild(newHeader);
+  
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
@@ -21,5 +49,6 @@ const headerAppender = (selector) => {
   // It should append the header to the element in the DOM that matches the given selector.
   //
 }
+
 
 export { Header, headerAppender }
